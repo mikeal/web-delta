230a27f5348b225b90d3e42ce5594fe791b3ea87
Mikeal Rogers <mikeal.rogers@gmail.com>
2022-10-06 14:00:58 +0100

    feat: fully working tree and deltas

--- test/test-file-tree.js
+++ test/test-file-tree.js
@@ -1,4 +1,6 @@
-import { FileTree } from '../tree.js'
+import { FileTree, Delta } from '../tree.js'
+import { deepEqual as same, ok } from 'assert'
+import { randomBytes as rand } from 'crypto'
 
 const fixture = Buffer.from('asdfasdf')
 
@@ -8,11 +10,42 @@ const testBasics = async () => {
   if (!Buffer.from(bytes).equals(fixture)) {
     throw new Error('rendered bytes do not match')
   }
-  
+
   bytes = Buffer.from(fixture.toString() + '00')
   const dest = await FileTree.fromBytes({ bytes })
-  const delta = ftree.delta(dest)
+  const delta = await ftree.delta(dest)
+  const carbytes = await delta.export()
 
+  const result = await ftree.apply(carbytes)
+  same(result, bytes)
 }
 
-testBasics()
+const testBig = async () => {
+  let bytes = rand(1024 * 1024 * 1024)
+
+  const ftree = await FileTree.fromBytes({ bytes })
+  const rendered_bytes = await ftree.read()
+  for (let i = 0; i < bytes.byteLength; i++) {
+    if (rendered_bytes[i] !== bytes[i]) throw new Error('not match ' + i)
+  }
+  same(rendered_bytes.byteLength, bytes.byteLength)
+  same(rendered_bytes, bytes)
+
+  const newbytes = Buffer.from(bytes)
+  const i = 1024 * 10
+  newbytes[i] = newbytes[i] === 0 ? 1 : newbytes[i] - 1
+
+  const dest = await FileTree.fromBytes({ bytes: newbytes })
+  const delta = await ftree.delta(dest)
+  const carbytes = await delta.export()
+  ok(carbytes.byteLength < 100000)
+
+  const result = await ftree.apply(carbytes)
+  same(result, newbytes)
+}
+
+const run = async () => {
+  await testBasics()
+  await testBig()
+}
+run()
--- tree.js
+++ tree.js
@@ -2,9 +2,44 @@ import * as dagPB from '@ipld/dag-pb'
 import * as Block from 'multiformats/block'
 import * as raw from 'multiformats/codecs/raw'
 import { sha256 as hasher } from 'multiformats/hashes/sha2'
-import { encodePB } from './node_modules/@ipld/unixfs/src/codec.js'
 import * as fs from 'node:fs/promises'
+import { readFileSync } from 'fs'
+import { PassThrough } from 'stream'
+import { createHash } from 'node:crypto'
+import { CarWriter, CarReader } from '@ipld/car'
+import { Readable } from 'stream'
+
+import { encodePB } from './vendor/codec.js'
 import cdc from './cdc.js'
+
+export const readCar = async carbytes => {
+  const car = await CarReader.fromBytes(carbytes)
+  const blockmap = new Map()
+  for await (const { cid, bytes } of car.blocks()) {
+    const codec = cid.code === 112 ? dagPB : raw
+    const block = Block.decode({ cid, bytes, codec, hasher })
+    blockmap.set(cid.toString(), block)
+  }
+  const [ root ] = await car.getRoots()
+  return { root, blockmap }
+}
+
+export const writeCar = async (root, blockmap, output) => {
+  const car = await CarWriter.create([root])
+  const out = Readable.from(car.out)
+  out.pipe(output)
+  const hasher = createHash('sha256')
+  out.pipe(hasher)
+  // sort the blocks by CID to ensure consistency
+  for (const key of [...blockmap.keys()].sort()) {
+    const block = blockmap.get(key)
+    car.writer.put(block)
+  }
+  await car.writer.close()
+  const hash = hasher.digest()
+  // TODO: build proper CID
+  return hash
+}
 
 /* IPLD encoding */
 const sumparts = parts => parts.reduce((size, { Tsize }) => Tsize + size, 0)
@@ -43,7 +78,10 @@ const createBranch = blocks => {
   return branch(parts)
 }
 
-// const isBreak = block => block.cid.bytes[block.cid.bytes.byteLength-1] === 0
+const isBreak = block => {
+  if (block.cid.code !== 112) throw new Error('method only for dagpb nodes')
+  block.cid.bytes[block.cid.bytes.byteLength-1] === 0
+}
 const getBreaks = blocks => {
   const breaks = []
   for (let i = 0; i < block.length; i++) {
@@ -106,26 +144,37 @@ export class Delta {
     this.blockmap = blockmap
   }
   static async fromDiff (sourceTree, destTree) {
-    const source = source
-    const dest = dest
-    const cids = new Set([...dest.cids].filter(cid => !source.cids.has(cid)))
-    const gb = cid => {
-      if (cid.startsWith('dagpb')) {
-        return destTree.tree.get(cid) || sourceTree.tree.get(cid)
-      } else cid.startsWith('cbor')) {
+    const cids = new Set([...destTree.cids].filter(cid => !sourceTree.cids.has(cid)))
+    const gb = async cid => {
+      if (cid.startsWith('bafy')) {
+        return [ cid, destTree.tree.get(cid) || sourceTree.tree.get(cid) ]
+      } else if (cid.startsWith('bafk')) {
         const chunk = destTree.chunkmap.get(cid) || sourceTree.chunkmap.get(cid)
-        return chunk.toBlock()
+        return [ cid, await chunk.toBlock() ]
       } else {
         throw new Error('unknown cid ' + cid)
       }
     }
 
-    const blockmap new Map(await Promise.all(cids.map.(gb)))
+    const blockmap = new Map(await Promise.all([...cids].map(gb)))
 
     return new this({ source: sourceTree.root, dest: destTree.root, blockmap })
   }
   proof () {
     return Proof.fromDelta(this)
+  }
+  async export (output) {
+    let buffers
+    if (!output) {
+      buffers = []
+      output = new PassThrough()
+      output.on('data', chunk => buffers.push(chunk))
+    }
+    await writeCar(this.dest, this.blockmap, output)
+    if (buffers) {
+      return Buffer.concat(buffers)
+    }
+    return output
   }
 }
 
@@ -139,11 +188,31 @@ export class FileTree {
     this.root = root
     this.cids = new Set([...chunkmap.keys(), ...tree.keys() ])
   }
+  getTreeBlock (cid) {
+    if (typeof cid !== 'string') cid = cid.toString()
+    return this.tree.get(cid)
+  }
+  getChunkBlock (cid) {
+    if (typeof cid !== 'string') cid = cid.toString()
+    const chunk = this.chunkmap.get(cid)
+    if (!chunk) return null
+    return chunk.toBlock()
+  }
+  getBlock (cid) {
+    if (typeof cid === 'string') {
+      if (cid.startsWith('bafy')) return this.getTreeBlock(cid)
+      else if (cid.startsWith('bafk')) return this.getChunkBlock(cid)
+      else throw new Error('Invalid block type')
+    }
+    if (cid.code === 112) return this.getTreeBlock(cid)
+    if (cid.code === 82) return this.getChunkBlock(cid)
+    throw new Error('Invalid block type')
+  }
   toByteVector ({ block, range }) {
     if (range) throw new Error('not implemented')
     if (!block) throw new Error('missingblock')
     const vector = [...block.links()].map(([,cid]) => {
-      if (cid.code === 113) {
+      if (cid.code === 112) {
         const block = this.tree.get(cid.toString())
         return this.toByteVector({ block })
       } else if (cid.code === 85) {
@@ -153,10 +222,10 @@ export class FileTree {
         throw new Error('unknown codec ' + cid.code)
       }
     })
-    return vector
+    return vector.flat()
   }
   async readVector (opts={}) {
-    if (!opts.block) opts.block = this.tree.get(this.root.toString())
+    if (!opts.block) opts.block = await this.getBlock(this.root)
     return Promise.all((await this.toByteVector(opts)).flat())
   }
   async read (opts={}) {
@@ -185,6 +254,7 @@ export class FileTree {
         chunkmap,
         filename
       }))
+      start = end
     }
     let tree = new Map()
 
@@ -202,7 +272,7 @@ export class FileTree {
     }
     if (part.length) leaves.push(await createLeaf(part))
     tree = new Map([...tree, ...leaves.map(block => [ block.cid.toString(), block ])])
-    
+
     let branches = leaves
     while (branches.length > 1) {
       const children = []
@@ -217,14 +287,55 @@ export class FileTree {
         }
       }
       if (part.length) children.push(await createBranch(part))
-      tree = new Map([...tree, children.map(block => [ block.cid.toString(), block ])])
+      tree = new Map([...tree, ...children.map(block => [ block.cid.toString(), block ])])
       branches = children
     }
+    const root = branches[0].cid
+    if (!tree.get(root.toString())) throw new Error('root missing from map')
 
-    return new this({ tree, chunks, chunkmap, filename, root: branches[0].cid })
+    return new this({ tree, chunks, chunkmap, filename, root })
   }
   async delta (dest) {
-    return Delta.fromDiff({ source, dest })
+    return Delta.fromDiff(this, dest)
+  }
+  async apply (carbytes, output) {
+    const { root, blockmap } = await readCar(carbytes)
+    let buffers
+    let size = 0
+    if (!output) {
+      buffers = []
+      output = new PassThrough()
+      output.on('data', chunk => buffers.push(chunk))
+    }
+    output.on('data', chunk => size += chunk.byteLength)
+
+    const walk = async cid => {
+      const scid = cid.toString()
+      const block = await ( this.getBlock(scid) || blockmap.get(scid) )
+      if (!block) throw new Error('Source file does not have enough origin data to apply delta')
+      const links = await Promise.all([...block.links()].map(async ([,link]) => {
+        const scid = link.toString()
+        const block = await this.getBlock(scid) || blockmap.get(scid)
+        if (!block) {
+          throw new Error('no block')
+        }
+        return block
+      }))
+      for (const block of links) {
+        if (block.cid.code === 112) await walk(block.cid)
+        else {
+          if (block.cid.code !== 85) {
+            // not a raw block
+            throw new Error('Bad DAG format, source not encoded in Web Delta')
+          }
+          output.write(block.bytes)
+        }
+      }
+    }
+    await walk(root)
+
+    if (buffers) return Buffer.concat(buffers)
+    return size
   }
 }
 
@@ -232,16 +343,26 @@ const processChunks = async chunks => {
   const blocks = await Promise.all(
     chunks.map(value => Block.encode({ codec: raw, hasher, value }))
   )
-  
+
   const remaining = blocks.slice(last + 1)
   return { blocks, leaves, remaining }
 }
 
-
+export const delta = async (source, dest) => {
+  if (typeof source === 'string') {
+    source = readFileSync(source)
+  }
+  if (typeof dest === 'string') {
+    dest = readFileSync(dest)
+  }
+  source = FileTree.fromBytes({ bytes: source })
+  dest = FileTree.fromBytes({ bytes: dest })
+  return source.delta(dest)
+}
 
 export const stat = async filename => {
   const blockmap = new Map()
-  
+
   const chunks = await readFile(filename)
   let { blocks, leaves, remaining } = await processChunks(chunks)
   leaves = await Promise.all([...leaves, await createLeaf(remaining)])
